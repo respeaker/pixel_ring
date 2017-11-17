27babb6e9f591390e89cd14f4959f8ae86ad6383
Yihui Xiong <yihui.xiong@hotmail.com>
2017-11-17 06:51:01 +0000

    add respeaker v2 example

---
+++ examples/respeaker_v2_pixel_ring.py
@@ -0,0 +1,38 @@
+"""
+Control pixel ring on ReSpeaker V2
+
+sudo apt install python-mraa libmraa1
+pip install pixel-ring
+
+"""
+
+import time
+
+from pixel_ring import pixel_ring
+import mraa
+
+en = mraa.Gpio(12)
+en.dir(mraa.DIR_OUT)
+en.write(0)
+
+
+if __name__ == '__main__':
+    while True:
+
+        try:
+            pixel_ring.wakeup()
+            time.sleep(3)
+            pixel_ring.think()
+            time.sleep(3)
+            pixel_ring.speak()
+            time.sleep(6)
+            pixel_ring.off()
+            time.sleep(3)
+        except KeyboardInterrupt:
+            break
+
+
+    pixel_ring.off()
+    time.sleep(1)
+
+en.write(1)
