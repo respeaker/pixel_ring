a8bf2ec809c75d92c03e358fd592b43331a5f949
Yihui Xiong <yihui.xiong@hotmail.com>
2018-01-22 11:37:22 +0800

    support respeaker usb 6+1 microphone array

---
+++ pixel_ring/apa102_pixel_ring.py
@@ -0,0 +1,90 @@
+
+import time
+import threading
+try:
+    import queue as Queue
+except ImportError:
+    import Queue as Queue
+
+from .apa102 import APA102
+from .pattern import Echo, GoogleHome
+
+
+class PixelRing(object):
+    PIXELS_N = 12
+
+    def __init__(self, pattern='google'):
+        if pattern == 'echo':
+            self.pattern = Echo(show=self.show)
+        else:
+            self.pattern = GoogleHome(show=self.show)
+
+        self.dev = APA102(num_led=self.PIXELS_N)
+
+        self.queue = Queue.Queue()
+        self.thread = threading.Thread(target=self._run)
+        self.thread.daemon = True
+        self.thread.start()
+        self.off()
+
+    def change_pattern(self, pattern):
+        if pattern == 'echo':
+            self.pattern = Echo(show=self.show)
+        else:
+            self.pattern = GoogleHome(show=self.show)
+
+    def wakeup(self, direction=0):
+        def f():
+            self.pattern.wakeup(direction)
+
+        self.put(f)
+
+    def listen(self):
+        self.put(self.pattern.listen)
+
+    def think(self):
+        self.put(self.pattern.think)
+
+    wait = think
+
+    def speak(self):
+        self.put(self.pattern.speak)
+
+    def off(self):
+        self.put(self.pattern.off)
+
+    def put(self, func):
+        self.pattern.stop = True
+        self.queue.put(func)
+
+    def _run(self):
+        while True:
+            func = self.queue.get()
+            self.pattern.stop = False
+            func()
+
+    def show(self, data):
+        for i in range(self.PIXELS_N):
+            self.dev.set_pixel(i, int(data[4*i + 1]), int(data[4*i + 2]), int(data[4*i + 3]))
+
+        self.dev.show()
+
+
+if __name__ == '__main__':
+    pixel_ring = PixelRing()
+    while True:
+        try:
+            pixel_ring.wakeup()
+            time.sleep(3)
+            pixel_ring.think()
+            time.sleep(3)
+            pixel_ring.speak()
+            time.sleep(6)
+            pixel_ring.off()
+            time.sleep(3)
+        except KeyboardInterrupt:
+            break
+
+
+    pixel_ring.off()
+    time.sleep(1)
---
+++ pixel_ring/pattern.py
@@ -0,0 +1,142 @@
+"""
+LED pattern like Echo
+"""
+
+import time
+
+
+class Echo(object):
+    def __init__(self, show, number=12):
+        self.pixels_number = number
+        self.pixels = [0] * 4 * number
+
+        if not callable(show):
+            raise ValueError('show parameter is not callable')
+
+        self.show = show
+        self.stop = False
+
+    def wakeup(self, direction=0):
+        position = int((direction + 15) / (360 / self.pixels_number)) % self.pixels_number
+
+        pixels = [0, 0, 0, 24] * self.pixels_number
+        pixels[position * 4 + 2] = 24
+
+        self.show(pixels)
+
+    def listen(self):
+        pixels = [0, 0, 0, 24] * self.pixels_number
+
+        self.show(pixels)
+
+    def think(self):
+        pixels  = [0, 0, 12, 12, 0, 0, 0, 24] * self.pixels_number
+
+        while not self.stop:
+            self.show(pixels)
+            time.sleep(0.2)
+            pixels = pixels[-4:] + pixels[:-4]
+
+    def speak(self):
+        step = 1
+        position = 12
+        while not self.stop:
+            pixels  = [0, 0, position, 24 - position] * self.pixels_number
+            self.show(pixels)
+            time.sleep(0.01)
+            if position <= 0:
+                step = 1
+                time.sleep(0.4)
+            elif position >= 12:
+                step = -1
+                time.sleep(0.4)
+
+            position += step
+
+    def off(self):
+        self.show([0] * 4 * 12)
+
+class GoogleHome(object):
+    def __init__(self, show):
+        self.basis = [0] * 4 * 12
+        self.basis[0 * 4 + 1] = 2
+        self.basis[3 * 4 + 1] = 1
+        self.basis[3 * 4 + 2] = 1
+        self.basis[6 * 4 + 2] = 2
+        self.basis[9 * 4 + 3] = 2
+
+        self.pixels = self.basis * 24
+
+        if not callable(show):
+            raise ValueError('show parameter is not callable')
+
+        self.show = show
+        self.stop = False
+
+    def wakeup(self, direction=0):
+        position = int((direction + 15) / 30) % 12
+
+        basis = self.basis[position*4:] + self.basis[:position*4]
+        for i in range(1, 25):
+            pixels = [v * i for v in basis]
+            self.show(pixels)
+            time.sleep(0.005)
+
+        pixels =  pixels[4:] + pixels[:4]
+        self.show(pixels)
+        time.sleep(0.1)
+
+        for i in range(2):
+            new_pixels = pixels[4:] + pixels[:4]
+            
+            self.show([v/2+pixels[index] for index, v in enumerate(new_pixels)])
+            pixels = new_pixels
+            time.sleep(0.1)
+
+        self.show(pixels)
+        self.pixels = pixels
+
+    def listen(self):
+        pixels = self.pixels
+        for i in range(1, 25):
+            self.show([(v * i / 24) for v in pixels])
+            time.sleep(0.01)
+
+    def think(self):
+        pixels = self.pixels
+
+        while not self.stop:
+            pixels = pixels[4:] + pixels[:4]
+            self.show(pixels)
+            time.sleep(0.2)
+
+        t = 0.1
+        for i in range(0, 5):
+            pixels = pixels[4:] + pixels[:4]
+            self.show([(v * (4 - i) / 4) for v in pixels])
+            time.sleep(t)
+            t /= 2
+
+        self.pixels = pixels
+
+    def speak(self):
+        pixels = self.pixels
+        step = 1
+        brightness = 5
+        while not self.stop:
+            self.show([(v * brightness / 24) for v in pixels])
+            time.sleep(0.02)
+
+            if brightness <= 5:
+                step = 1
+                time.sleep(0.4)
+            elif brightness >= 24:
+                step = -1
+                time.sleep(0.4)
+
+            brightness += step
+
+    def off(self):
+        self.show([0] * 4 * 12)
+
+
